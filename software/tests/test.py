ef53845190062d6f1376d200f59fa7faf3993ad9
Olaf Lüke <olaf@tinkerforge.com>
2018-03-07 16:53:22 +0100

    Add simple test file

---
+++ software/tests/test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+HOST = "localhost"
+PORT = 4223
+UID = "abc1" 
+
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.bricklet_industrial_counter import BrickletIndustrialCounter
+import time
+
+if __name__ == "__main__":
+    ipcon = IPConnection() # Create IP connection
+    counter = BrickletIndustrialCounter(UID, ipcon) # Create device object
+
+    ipcon.connect(HOST, PORT) # Connect to brickd
+    # Don't use device before ipcon is connected
+
+
+#    counter.set_counter(counter.PIN_0, )
+
+    counter.set_counter_configuration(counter.PIN_1, 
+                                      counter.COUNT_EDGE_RISING,
+                                      counter.COUNT_DIRECTION_UP,
+                                      counter.DUTY_CYCLE_PRESCALER_1,
+                                      counter.FREQUENCY_INTEGRATION_TIME_256_MS)
+
+    while True:
+        t = time.time()
+        signal_data = counter.get_signal_data(1)
+        count = counter.get_counter(1)
+        print("Count: {}".format(count))
+        print("Signal Data: {}".format(signal_data))
+#        print(signal_data.frequency - signal_data.frequency % 10000000)
+        time.sleep(0.2 - (time.time()-t))
+
+    raw_input("Press key to exit\n") # Use input() in Python 3
+    ipcon.disconnect()
