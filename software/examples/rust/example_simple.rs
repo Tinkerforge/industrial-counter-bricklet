c70f16d8b45b5156e34da78ffb416266e39e4548
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:34:42 +0100

    rust: Reformat examples.

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -14,15 +14,25 @@ fn main() -> Result<(), Box<dyn Error>> {
                                           // Don't use device before ipcon is connected.
 
     // Get current counter from channel 0.
-    let counter = ic.get_counter(INDUSTRIAL_COUNTER_BRICKLET_CHANNEL_0).recv()?;
+    let counter = ic
+        .get_counter(INDUSTRIAL_COUNTER_BRICKLET_CHANNEL_0)
+        .recv()?;
     println!("Counter (Channel 0): {}", counter);
 
     // Get current signal data from channel 0.
-    let signal_data = ic.get_signal_data(INDUSTRIAL_COUNTER_BRICKLET_CHANNEL_0).recv()?;
+    let signal_data = ic
+        .get_signal_data(INDUSTRIAL_COUNTER_BRICKLET_CHANNEL_0)
+        .recv()?;
 
-    println!("Duty Cycle (Channel 0): {} %", signal_data.duty_cycle as f32 / 100.0);
+    println!(
+        "Duty Cycle (Channel 0): {} %",
+        signal_data.duty_cycle as f32 / 100.0
+    );
     println!("Period (Channel 0): {} ns", signal_data.period);
-    println!("Frequency (Channel 0): {} Hz", signal_data.frequency as f32 / 1000.0);
+    println!(
+        "Frequency (Channel 0): {} Hz",
+        signal_data.frequency as f32 / 1000.0
+    );
     println!("Value (Channel 0): {}", signal_data.value);
 
     println!("Press enter to exit.");
