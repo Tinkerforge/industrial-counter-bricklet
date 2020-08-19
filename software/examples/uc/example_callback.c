5f0cbb9383afb5caf96944a570f7695c222cfaaf
Erik Fleckstein <erik@tinkerforge.com>
2020-08-19 16:51:24 +0200

    Add uc examples.

---
+++ software/examples/uc/example_callback.c
@@ -0,0 +1,48 @@
+#define __STDC_FORMAT_MACROS // for PRId64/PRIu64 in C++
+
+
+#include <inttypes.h>
+
+#include "bindings/hal_common.h"
+#include "bindings/bricklet_industrial_counter.h"
+
+#define UID "XYZ" // Change XYZ to the UID of your Industrial Counter Bricklet
+
+void check(int rc, const char* msg);
+
+
+
+// Callback function for all counter callback
+void all_counter_handler(TF_IndustrialCounter *device, int64_t counter[4],
+                         void *user_data) {
+	(void)device; (void)user_data; // avoid unused parameter warning
+
+	tf_hal_printf("Counter (Channel 0): %" PRId64 "\n", counter[0]);
+	tf_hal_printf("Counter (Channel 1): %" PRId64 "\n", counter[1]);
+	tf_hal_printf("Counter (Channel 2): %" PRId64 "\n", counter[2]);
+	tf_hal_printf("Counter (Channel 3): %" PRId64 "\n", counter[3]);
+	tf_hal_printf("\n");
+}
+
+
+TF_IndustrialCounter ic;
+
+void example_setup(TF_HalContext *hal) {
+	// Create device object
+	check(tf_industrial_counter_create(&ic, UID, hal), "create device object");
+
+
+	// Register all counter callback to function all_counter_handler
+	tf_industrial_counter_register_all_counter_callback(&ic,
+	                                                   all_counter_handler,
+	                                                   NULL);
+
+	// Set period for all counter callback to 1s (1000ms)
+	tf_industrial_counter_set_all_counter_callback_configuration(&ic, 1000, true);
+
+}
+
+void example_loop(TF_HalContext *hal) {
+	// Poll for callbacks
+	tf_hal_callback_tick(hal, 0);
+}
---
+++ software/examples/uc/example_simple.c
@@ -0,0 +1,46 @@
+#define __STDC_FORMAT_MACROS // for PRId64/PRIu64 in C++
+
+
+#include <inttypes.h>
+
+#include "bindings/hal_common.h"
+#include "bindings/bricklet_industrial_counter.h"
+
+#define UID "XYZ" // Change XYZ to the UID of your Industrial Counter Bricklet
+
+void check(int rc, const char* msg);
+
+
+
+
+TF_IndustrialCounter ic;
+
+void example_setup(TF_HalContext *hal) {
+	// Create device object
+	check(tf_industrial_counter_create(&ic, UID, hal), "create device object");
+
+
+	// Get current counter from channel 0
+	int64_t counter;
+	check(tf_industrial_counter_get_counter(&ic, TF_INDUSTRIAL_COUNTER_CHANNEL_0,
+	                                        &counter), "get counter from channel 0");
+
+	tf_hal_printf("Counter (Channel 0): %" PRId64 "\n", counter);
+
+	// Get current signal data from channel 0
+	uint16_t duty_cycle; uint64_t period; uint32_t frequency; bool value;
+	check(tf_industrial_counter_get_signal_data(&ic, TF_INDUSTRIAL_COUNTER_CHANNEL_0,
+	                                            &duty_cycle, &period, &frequency,
+	                                            &value), "get signal data from channel 0");
+
+	tf_hal_printf("Duty Cycle (Channel 0): %d 1/%d %%\n", duty_cycle, 100.0);
+	tf_hal_printf("Period (Channel 0): %" PRIu64 " ns\n", period);
+	tf_hal_printf("Frequency (Channel 0): %d 1/%d Hz\n", frequency, 1000.0);
+	tf_hal_printf("Value (Channel 0): %s\n", value ? "true" : "false");
+
+}
+
+void example_loop(TF_HalContext *hal) {
+	// Poll for callbacks
+	tf_hal_callback_tick(hal, 0);
+}
